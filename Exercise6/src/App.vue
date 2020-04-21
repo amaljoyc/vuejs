d3c4b48e6ae453d3f35b4edbad26c2976d8ba8b9
amaljoyc <amaljoyc@gmail.com>
2020-04-21 22:03:52 +0200

    exercise 6

---
+++ Exercise6/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <container></container>
+</template>
+
+<script>
+import Container from './components/Container.vue';
+
+export default {
+    components: {
+        'container': Container
+    }
+}
+</script>
+
+<style></style>
---
+++ Exercise6/src/components/Container.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <my-header></my-header>
+    <hr />
+    <server></server>
+    <hr />
+    <my-footer></my-footer>
+  </div>
+</template>
+
+<script>
+import Header from './Header.vue';
+import Server from './Server.vue';
+import Footer from './Footer.vue';
+
+export default {
+    components: {
+        'my-header': Header,
+        'server': Server,
+        'my-footer': Footer,
+    }
+};
+</script>
